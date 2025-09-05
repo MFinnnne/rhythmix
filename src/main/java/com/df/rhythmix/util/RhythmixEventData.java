97db8439394e65d67af2998571059de17d10e9c8
MFine <lxemyf@gmail.com>
2025-09-05 17:48:55 +0800

    Enhance README.md with additional examples and documentation for RhythmixEventData; update AviatorConfig to support numeric type comparisons; add comprehensive test cases for comparison expressions.

--- src/main/java/com/df/rhythmix/util/RhythmixEventData.java
+++ src/main/java/com/df/rhythmix/util/RhythmixEventData.java
@@ -1,6 +1,10 @@
 package com.df.rhythmix.util;
 
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.IdcardUtil;
+import cn.hutool.core.util.RandomUtil;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +13,7 @@ import java.sql.Timestamp;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class RhythmixEventData {
 
     public RhythmixEventData(String id, String name, String value, Timestamp ts) {
@@ -18,7 +23,16 @@ public class RhythmixEventData {
         this.value = value;
     }
 
+    public RhythmixEventData(String name, String value, Timestamp ts) {
+        this.id = IdUtil.nanoId();
+        this.name = name;
+        this.value = value;
+        this.ts = ts;
+    }
+
     private String id;
+    private String code;
+    private String serialNumber;
     private String name;
     private String value;
     private Timestamp ts;
